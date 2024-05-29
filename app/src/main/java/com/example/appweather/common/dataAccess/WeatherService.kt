4d2916df09929f3968f82165e5e2b343c362dd29
Matias <matias_r12@hotmail.com>
2024-05-29 10:23:46 -0300

    Servicio de RETROFIT

---
+++ app/src/main/java/com/example/appweather/common/dataAccess/WeatherService.kt
@@ -0,0 +1,18 @@
+package com.example.appweather.common.dataAccess
+
+import com.example.appweather.common.WeatherForecastEntity
+import com.example.appweather.common.utils.Constants
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherService {
+    @GET(Constants.ONE_CALL_PATH)
+    suspend fun getWeatherForecastByCoordinates(
+        //pasar parametros
+        @Query (Constants.LATITUDE_PARAM) lat: Double,
+        @Query (Constants.LATITUDE_PARAM) long: Double,
+        @Query (Constants.APP_ID_PARAM) appId: String,
+        @Query (Constants.UNITS_PARAM) units: String,
+        @Query (Constants.LANGUAGE_PARAM) lang: String
+    ) : WeatherForecastEntity
+}
---
+++ app/src/main/java/com/example/appweather/common/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.appweather.common.utils
+
+object Constants {
+    const val BASE_URL = "https://api.openweathermap.org/"
+    const val ONE_CALL_PATH = "data/3.0/onecall" //complemento del servicio
+
+    const val LATITUDE_PARAM = "lat"
+    const val LONGITUDE_PARAM = "lon"
+    const val APP_ID_PARAM = "appid"
+    const val UNITS_PARAM = "units"
+    const val LANGUAGE_PARAM = "lang"
+}
