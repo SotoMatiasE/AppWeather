10acd67f53c505260a8b7795cfc02daf461c20e9
Matias <matias_r12@hotmail.com>
2024-05-29 08:49:37 -0300

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    kotlin("kapt")
+}
+
+android {
+    namespace = "com.example.appweather"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.appweather"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    dataBinding {
+        enable = true
+    }
+}
+
+val androidxCoreVersion = "1.12.0"
+val appcompatVersion = "1.6.1"
+val materialVersion = "1.10.0"
+val constraintVersion = "2.1.4"
+val glideVersion = "4.16.0"
+val roomVersion = "2.6.1"
+val lifecycleVersion = "2.6.2"
+val coroutinesVersion = "1.7.2"
+val activityVersion = "1.8.2"
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    //Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+
+    // Kotlin-Activity
+    implementation("androidx.activity:activity-ktx:$activityVersion")
+
+    //Retroft
+    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
